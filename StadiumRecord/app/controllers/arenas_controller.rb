11773137ea3c6c73f38bf6898f6a4453ed69b8b1
Mark A Guza <guzamaguza@gmail.com>
2020-08-04 01:23:04 +0000

    updated

--- StadiumRecord/app/controllers/arenas_controller.rb
+++ StadiumRecord/app/controllers/arenas_controller.rb
@@ -6,6 +6,21 @@ class ArenasController < ApplicationController
       @user = current_user
       @arenas = @user.arenas
 
+      @arena_arr = []
+      @arenas.each do |x|
+        @arena_arr << x.name
+      end
+
+      @arena_uniq_hash = {}
+      @uniq_arenas = @arena_arr.uniq
+      @@arena_hash.each_with_index do |value, index|
+        @uniq_arenas.each do |aren|
+          if aren == value[1][:name]
+            @arena_uniq_hash[:"#{aren}"] = value[1]
+          end
+        end
+      end
+
       erb :'arenas/index'
     end
 
