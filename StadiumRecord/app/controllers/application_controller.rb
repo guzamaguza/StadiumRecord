eadaa1ff90fbe9bcb6011cff4d9f5acea76c86f4
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2020-08-04 17:38:41 -0700

    Update application_controller.rb

--- StadiumRecord/app/controllers/application_controller.rb
+++ StadiumRecord/app/controllers/application_controller.rb
@@ -9,38 +9,19 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "stadiumrecord_secret"
-    register Sinatra::Flash
   end
 
   get '/' do
-    if logged_in?
-      @user = current_user
-      @arenas = @@arena_hash
-      erb :'/visits/index'
-    else
-      @user = current_user
-      @arenas = @@arena_hash
-      erb :welcome
-    end
-  end
-
-  get '/list_of_arenas' do
-      if logged_in?
-          @arenas = @@arena_hash
-          erb :list_of_arenas
-      else
-          redirect '/'
-      end
+    @arenas = Arena.all
+    erb :welcome
   end
 
   helpers do
 
-    #returns a boolean if the user is loggedIn or not (? means boolean return)
     def logged_in?
       !!current_user
     end
 
-    #keeps track of the logged in user
     def current_user
       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
