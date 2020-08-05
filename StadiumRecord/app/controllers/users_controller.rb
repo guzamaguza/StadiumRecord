68f167cee5f4343c1cdb949562e5b7ed57100136
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2020-08-04 17:39:36 -0700

    Update users_controller.rb

--- StadiumRecord/app/controllers/users_controller.rb
+++ StadiumRecord/app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
 
-=begin
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
     erb :'users/show'
   end
-=end
 
   get '/signup' do
     if !logged_in?
@@ -16,26 +14,12 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    #can't use mass assignment here
     if params[:username] == "" || params[:email] == "" || params[:password] == ""
-      flash[:error_missinginfo] = "Make Sure to Fill In All Boxes to Signup!"
-
       redirect to '/signup'
     else
-      @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
-      session[:user_id] = @user.id
-
-      if @user.save
-        flash[:success_newuser] = "New User Created Successfully!"
-
-        redirect "/visits"
-      else
-        flash[:error_newuser] = "New User Creation Failed: #{@user.errors.full_messages.to_sentence}"
-
-        redirect "/signup"
-      end
-
-      redirect "/signup"
+      user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+      session[:user_id] = user.id
+      redirect to '/visits'
     end
   end
 
@@ -49,22 +33,18 @@ class UsersController < ApplicationController
 
   post '/login' do
     user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password]) #authenticate compares pwd to bcrypt salted/hashed pwd
+    if user && user.authenticate(:password => params[:password])
       session[:user_id] = user.id
-      flash[:success_login] = "Successfully Signed In!"
-
       redirect to "/visits"
     else
-      flash[:error_invalidcred] = "Invalid Credentials, Try Again!"
-
-      redirect to '/login'
+      redirect to '/signup'
     end
   end
 
   get '/logout' do
     if logged_in?
-      #session.delete(:message)
-      session.clear
+      session.delete(:message)
+      session.destroy
       redirect to '/login'
     else
       redirect to '/'
